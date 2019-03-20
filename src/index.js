737bb355b440b82c37d42b843e50bec20dd60e09
Diego Borges <drborges.cic@gmail.com>
2019-03-20 09:35:06 -0300

    Spike on subscriptions

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import gql from "graphql-tag";
 import { ApolloClient } from "apollo-client";
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloProvider, ApolloConsumer, Query, Mutation } from "react-apollo";
+import { HttpLink } from "apollo-link-http";
+import { WebSocketLink } from "apollo-link-ws";
+import { split } from "apollo-link";
+import { getMainDefinition } from "apollo-utilities";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider, Query, Mutation, Subscription } from "react-apollo";
 
 import "./styles.css";
 
@@ -16,7 +19,7 @@ export const typeDefs = gql`
   extend type Mutation {
     toggleUserBlockState(id: Int!): User!
   }
-`
+`;
 
 const FETCH_USERS = gql`
   query FetchUsers {
@@ -28,7 +31,7 @@ const FETCH_USERS = gql`
       blocked @client
     }
   }
-`
+`;
 
 const TOGGLE_USER_BLOCK_STATE = gql`
   mutation ToggleUserBlockState($id: Int!) {
@@ -37,12 +40,44 @@ const TOGGLE_USER_BLOCK_STATE = gql`
       blocked @client
     }
   }
-`
+`;
 
-const cache = new InMemoryCache()
-const link = new HttpLink({
-  uri: "https://b1a56f31.ngrok.io/graphql",
-})
+const USER_SUBSCRIPTION = gql`
+  subscription onUserAdded {
+    userAdded {
+      id
+      name
+      email
+      age
+      blocked @client
+    }
+  }
+`;
+
+const cache = new InMemoryCache();
+const sandboxId = "kop4j51nkr";
+const wsLink = new WebSocketLink({
+  uri: `wss://${sandboxId}.sse.codesandbox.io/graphql`,
+  options: {
+    reconnect: true
+  }
+});
+
+const httpLink = new HttpLink({
+  uri: `https://${sandboxId}.sse.codesandbox.io/graphql`
+});
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  httpLink
+);
 
 const client = new ApolloClient({
   link,
@@ -55,83 +90,73 @@ const client = new ApolloClient({
     User: {
       blocked: (user, _, { cache }) => {
         const { users } = cache.readQuery({ query: FETCH_USERS });
-        const userFromCache = users.find(u => u.id === user.id)
-        return userFromCache !== undefined && userFromCache.blocked
-      },
+        const userFromCache = users.find(u => u.id === user.id);
+        return userFromCache !== undefined && userFromCache.blocked;
+      }
     },
     Mutation: {
       toggleUserBlockState: (user, { id }, { cache }) => {
-        const data = cache.readQuery({ query: FETCH_USERS })
-        const { users } = data
-        const userFromCache = users.find(u => u.id === id)
-        userFromCache.blocked = !userFromCache.blocked
-        cache.writeQuery({ query: FETCH_USERS, data })
+        const data = cache.readQuery({ query: FETCH_USERS });
+        const { users } = data;
+        const userFromCache = users.find(u => u.id === id);
+        userFromCache.blocked = !userFromCache.blocked;
+        cache.writeQuery({ query: FETCH_USERS, data });
       }
     }
-  },
-})
+  }
+});
 
 cache.writeData({
   data: {
-    users: [],
-  },
+    users: []
+  }
 });
 
-
-function App() {
+const App = () => {
   return (
     <ApolloProvider client={client}>
-      <Query
-          query={FETCH_USERS}
-          fetchPolicy="network-only"
-      >
-        {({ loading, error, data, refetch }) => {
-          if (loading) return <li>Loading...</li>;
-          if (error) return <li>Error :(</li>;
+      <ul>
+        <Query query={FETCH_USERS} fetchPolicy="network-only">
+          {({ loading, error, data, subscribeToMore }) => {
+            if (loading) return <li>Loading...</li>;
+            if (error) return <li>Error :(</li>;
 
-          return (
-            <div className="App">
-              <ul>
-                {data.users.map(user => (
-                  <Mutation
-                    mutation={TOGGLE_USER_BLOCK_STATE}
-                    variables={{ id: user.id }}
-                    refetchQueries={FETCH_USERS}
+            subscribeToMore({
+              document: USER_SUBSCRIPTION,
+              updateQuery: (prev, { subscriptionData }) => {
+                if (!subscriptionData.data) return prev;
+                const newUser = subscriptionData.data.userAdded;
+
+                return Object.assign({}, prev, {
+                  entry: {
+                    users: [...prev.entry.comments, newUser]
+                  }
+                });
+              }
+            });
+
+            return data.users.map(user => (
+              <Mutation
+                key={user.id}
+                mutation={TOGGLE_USER_BLOCK_STATE}
+                variables={{ id: user.id }}
+              >
+                {mutate => (
+                  <li
+                    className={`item ${user.blocked ? "blocked" : "available"}`}
+                    onClick={mutate}
                   >
-                    {mutate => (
-                      <li
-                          key={user.id}
-                          className={`item ${user.blocked ? "blocked" : "available"}`}
-                          onClick={mutate}
-                      >
-                        {user.name}
-                      </li>
-                    )}
-                  </Mutation>
-                ))}
-              </ul>
-              <button type="button" onClick={() => refetch()}>Refresh</button>
-            </div>
-          )
-        }}
-      </Query>
+                    {user.name}
+                  </li>
+                )}
+              </Mutation>
+            ));
+          }}
+        </Query>
+      </ul>
     </ApolloProvider>
   );
-}
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
